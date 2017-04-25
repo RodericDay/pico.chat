523c212489dc85803c292311dcc1cd7a0235703d
Roderic Day <roderic.day@gmail.com>
2017-04-25 18:48:56 -0400

    TypeScript Pt. 1

--- client/ts/websockets.ts
+++ client/ts/websockets.ts
@@ -6,16 +6,19 @@ function createWebSocket() {
     ws.onopen = onOpen;
     ws.onclose = onClose;
     ws.onmessage = onMessage;
-    return ws;
+    return ws
 }
+
 function onOpen(event) {
     ws.send('Hi! I am ' + uid);
     updateUi({});
 }
+
 function onClose(event) {
-    messages.push({ type: "info", sender: "server", text: "logout" });
+    messages.push({type: "info", sender: "server", text: "logout"});
     updateUi({});
 }
+
 function onMessage(event) {
     var components = event.data.split(': ');
     var sender = components.shift();
@@ -24,36 +27,45 @@ function onMessage(event) {
         var message = JSON.parse(data);
         message.sender = sender;
     }
-    catch (error) {
+    catch(error) {
     }
-    if (sender === 'Welcome! Users') {
+
+    if(sender === 'Welcome! Users') {
         users = new Set([...users, ...data.split(',')]);
         users.delete("");
-        messages.push({ type: "info", sender: "server", text: "login" });
+        messages.push({type: "info", sender: "server", text: "login"});
     }
-    else if (event.data.match(/^[^:]* joined/)) {
+
+    else if(event.data.match(/^[^:]* joined/)) {
         var name = event.data.replace(/ .*/, '');
         users.add(name);
-        messages.push({ type: "info", sender: name, text: "connect" });
+        messages.push({type: "info", sender: name, text: "connect"});
+
     }
-    else if (event.data.match(/^[^:]* disconnected/)) {
+
+    else if(event.data.match(/^[^:]* disconnected/)) {
         var name = event.data.replace(/ .*/, '');
         users.delete(name);
-        messages.push({ type: "info", sender: name, text: "disconnect" });
+        messages.push({type: "info", sender: name, text: "disconnect"});
     }
-    else if (message) {
+
+    else if(message) {
         messages.push(message);
     }
+
     else {
         alert(event.data + '. Refresh page to try again.');
-        messages.push({ type: "error", sender: "system", text: event.data });
+        messages.push({type: "error", sender: "system", text: event.data});
     }
-    updateUi(messages[messages.length - 1]);
+
+    updateUi(messages[messages.length-1]);
 }
+
 function updateUi(lastMessage) {
     console.log(lastMessage);
     console.log("Implement a function to handle updates!");
 }
+
 var uid = Math.random().toFixed(16).slice(2, 8);
 var users = new Set();
 var messages = [];
