7d197401b675a012fac0802ed02492e0f95b4be9
Roderic Day <roderic.day@gmail.com>
2017-04-25 19:18:44 -0400

    TypeScript Pt. 2

--- client/ts/webrtc.ts
+++ client/ts/webrtc.ts
@@ -1,71 +1,87 @@
-var myRPCs = {};
-var myStreams = {};
+interface RPCCollection {[key: string]: RTCPeerConnection}
+interface StreamCollection {[key: string]: MediaStream}
+
+var myRPCs: RPCCollection = {};
+var myStreams: StreamCollection = {};
+
 function getRPC(emitter, remoteAgentName) {
     var rpc = myRPCs[remoteAgentName];
-    if (rpc === undefined) {
-        var servers = { iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] };
+    if(rpc===undefined) {
+        var servers = {iceServers: [{urls:['stun:stun.l.google.com:19302']}]};
         var rpc = new RTCPeerConnection(servers);
-        rpc.oniceconnectionstatechange = function (e) {
+        rpc.oniceconnectionstatechange = function(e) {
             console.log(`(${remoteAgentName}) ${rpc.iceConnectionState}`);
-            if (['closed', 'disconnected'].includes(rpc.iceConnectionState)) {
+            if(['closed', 'disconnected'].includes(rpc.iceConnectionState)) {
                 delete myRPCs[remoteAgentName];
                 delete myStreams[remoteAgentName];
+
                 var left = Object.keys(myStreams);
-                if (left.length === 1 && left[0] === uid) {
-                    myStreams[uid].getTracks().forEach(track => track.stop());
+                if(left.length === 1 && left[0] === uid) {
+                    myStreams[uid].getTracks().forEach(track=>track.stop());
                     delete myRPCs[uid];
                     delete myStreams[uid];
                 }
+
                 updateUi({});
             }
-        };
-        rpc.onicecandidate = e => onIceCandidate(rpc, emitter, remoteAgentName);
-        rpc.ontrack = e => setupStream(e.streams[0], remoteAgentName);
+        }
+        rpc.onicecandidate = e=>onIceCandidate(rpc, emitter, remoteAgentName);
+        (rpc as any).ontrack = e=>setupStream(e.streams[0], remoteAgentName);
         rpc.addStream(myStreams[uid]);
         myRPCs[remoteAgentName] = rpc;
     }
-    return rpc;
+    return rpc
 }
+
 function connect(emitter, remoteAgentName, message) {
     /* emitter should be a function places message somewhere for collection */
-    if (!myStreams[uid]) {
-        createMyStream().then(_ => connect(emitter, remoteAgentName, message));
-        return;
+
+    if(!myStreams[uid]) {
+        createMyStream().then(_=>connect(emitter, remoteAgentName, message));
+        return
     }
+
     var rpc = getRPC(emitter, remoteAgentName);
-    if (message.type === 'request') {
+
+    if(message.type==='request') {
         console.log(`(${remoteAgentName}) requesting`);
         rpc.createOffer()
-            .then(offer => rpc.setLocalDescription(offer))
-            .catch(error => console.log(error));
+            .then(offer=>rpc.setLocalDescription(offer))
+            .catch(error=>console.log(error));
     }
-    else if (message.type === 'offer') {
+
+    else if(message.type==='offer') {
         rpc.setRemoteDescription(new RTCSessionDescription(message));
         console.log(`(${remoteAgentName}) connecting`);
         rpc.createAnswer()
-            .then(answer => rpc.setLocalDescription(answer))
-            .catch(error => console.log(error));
+            .then(answer=>rpc.setLocalDescription(answer))
+            .catch(error=>console.log(error));
     }
-    else if (message.type === 'answer') {
+
+    else if(message.type==='answer') {
         console.log(`(${remoteAgentName}) connecting`);
         rpc.setRemoteDescription(new RTCSessionDescription(message))
-            .catch(error => console.log(error));
+            .catch(error=>console.log(error));
     }
+
 }
+
 function createMyStream() {
-    var settings = { audio: true, video: { width: 320, height: 240 } };
+    var settings = {audio:true,video:{width:320,height:240}};
     // var settings = {audio:true,video:false};
     return navigator.mediaDevices
         .getUserMedia(settings)
-        .then(stream => setupStream(stream, uid))
-        .catch(error => console.log(error));
+        .then(stream=>setupStream(stream, uid))
+        .catch(error=>console.log(error));
 }
+
 function setupStream(stream, agentName) {
     myStreams[agentName] = stream;
     updateUi({});
 }
+
 function onIceCandidate(rpc, emitter, remoteAgentName) {
-    if (rpc.iceGatheringState === 'complete') {
+    if(rpc.iceGatheringState==='complete') {
         var message = {
             target: remoteAgentName,
             type: rpc.localDescription.type,
