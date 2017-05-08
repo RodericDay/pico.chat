e739486c9882d68e9daa1d3c6e5d3cdfe184fb1a
Roderic Day <roderic.day@gmail.com>
2017-05-08 11:51:18 -0400

    Overhaul

---
+++ client/ts/chat.ts
@@ -0,0 +1,125 @@
+var config = {
+    wsUrl: "wss://chat.roderic.ca/ws/",
+}
+var state = {
+    loggedIn: false,
+    username: localStorage.username||"",
+    users: new Set(),
+    messages: [],
+    errors: [],
+    actions: [logout, clear],
+    ws: null,
+}
+function login(e) {
+    e.preventDefault();
+    state.username = e.target.username.value;
+    localStorage.username = state.username;
+    openConnection();
+}
+function openConnection() {
+    state.ws = new WebSocket(config.wsUrl);
+    state.ws.onopen = (e) => {
+        state.ws.send(`Hi! I am ${state.username}`);
+        m.redraw();
+    }
+    state.ws.onclose = (e) => {
+        state.loggedIn = false;
+        m.redraw();
+    }
+    state.ws.onmessage = (e) => {
+        if(e.data.match(/^Welcome!.*/)) {
+            state.loggedIn = true;
+            var string = e.data.split(': ')[1];
+            if(string) { state.users = new Set([...string.split(', ')]); }
+        }
+        else if(e.data.match(/^User already exists$/)) {
+            state.errors.push(`${state.username} is taken. Try a different username?`);
+        }
+        else if(e.data.match(/^Name cannot .+ be empty$/)) {
+            state.errors.push(e.data);
+        }
+        else if(e.data.match(/^\w+ joined$/)) {
+            state.users.add(e.data.match(/^\S+/)[0]);
+        }
+        else if(e.data.match(/^\w+ disconnected$/)) {
+            state.users.delete(e.data.match(/^\S+/)[0]);
+        }
+        else {
+            try {
+                var special = JSON.parse(e.data.split(': ').slice(1).join(': '));
+                if(special.type) {
+                    console.log(special.type);
+                    window.dispatchEvent(new CustomEvent(special.type, {detail: special}));
+                }
+                else {
+                    throw "not a properly formatted message, dump text";
+                }
+            }
+            catch(error) {
+                state.messages.push(e.data);
+            }
+        }
+        localStorage.history = JSON.stringify(state.messages);
+        m.redraw();
+    }
+}
+function clear(e) {
+    var msg = `You sure you want to delete ${state.messages.length} messages?`
+    if(confirm(msg)) {
+        localStorage.history = JSON.stringify([]);
+        refresh();
+    }
+}
+function logout(e) {
+    delete localStorage.username;
+    refresh();
+}
+function post(e) {
+    e.preventDefault();
+    if(e.target.post.value) {
+        state.ws.send(e.target.post.value);
+        e.target.post.value = "";
+    }
+}
+function refresh() {
+    location.replace(location.href);
+}
+function scrollToNewest() {
+    window.setTimeout(function(){
+        var el = document.getElementById("chat-log");
+        if(el) {
+            el.scrollTop = el.scrollHeight;
+        }
+    }, 0);
+    return []
+}
+var viewErrors = () => m("div", state.errors.map((s,i)=>
+        m("div.error", {onclick: ()=>state.errors.splice(i, 1)}, s))
+    );
+var viewLogin = () => m("form", {onsubmit: login}, [
+        m("input[name=username]", {value: state.username, autocomplete: "off"}),
+    ]);
+var viewActions = () => m("div#actions", state.actions.slice(0).reverse().map(f=>
+        m("button", {onclick: f}, f.name),
+    ));
+var viewChatLog = () => m("div#chat-log", state.messages.map(s=>m.trust(marked(s))));
+var viewInput = () => m("form", {onsubmit: post}, [
+        m("input[name=post]", {autocomplete: "off"}),
+    ]);
+var viewUserlist = () => m("div#user-list", [...state.users].sort().map(u=>m("div", u)));
+var Chat = {
+    view: () =>
+        state.loggedIn
+        ?    [viewErrors(), viewActions(), viewChatLog(), viewInput(), viewUserlist(), scrollToNewest()]
+        :    [viewErrors(), viewLogin()]
+}
+try {
+    state.messages = JSON.parse(localStorage.history);
+    if(state.messages.constructor.name !== "Array") { throw "storage error" }
+}
+catch(e) {
+    state.messages = [];
+}
+var root = document.getElementById("chat");
+m.mount(root, Chat);
+if(state.username) { openConnection(); }
