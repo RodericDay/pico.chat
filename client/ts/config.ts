cce289f34cf2dbec8a7adc20e9f7f0fb58c26945
Roderic Day <roderic.day@gmail.com>
2017-08-18 20:24:32 -0400

    Point to my own server

--- client/ts/config.ts
+++ client/ts/config.ts
@@ -2,6 +2,8 @@ let config = {
     wsUrl: location.href.match("http://localhost:8000/")
         ? "ws://localhost:9160/"
         : "wss://chat.roderic.ca/ws/",
-    iceServers: [{urls: ['stun:stun.l.google.com:19302']}],
+    iceServers: [
+        {urls: ["159.203.33.68:3478"], username: 'test', password: 'test'},
+    ],
     media: {audio: true, video: {width: 320, height: 240, facingMode: "user"}},
 }
