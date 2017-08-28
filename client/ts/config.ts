92aecb13f4cc25e9bc645bb1c76e657f1b7a16ac
Roderic Day <roderic.day@gmail.com>
2017-08-28 14:15:36 -0400

    TURN server

--- client/ts/config.ts
+++ client/ts/config.ts
@@ -4,6 +4,7 @@ let config = {
         : "wss://permanentsignal.com/ws/",
     iceServers: [
         {urls: ['stun:stun.l.google.com:19302']},
+        {urls: ['turn:159.203.33.68:3478'], username: 'bionic', credential: 'hunter2'},
     ],
     media: {audio: true, video: {width: 320, height: 240, facingMode: "user"}},
 }
